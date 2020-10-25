7f83f63250279382946bd26acfe0d1a095d92256
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2020-10-25 11:22:40 -0500

    Adding groupMapReduce method to Iterator

---
+++ src/main/scala/scala/collection/IteratorExtensions.scala
@@ -0,0 +1,40 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.collection
+
+object IteratorExtensions {
+  implicit class IteratorExtensionsOps[A](private val iter: Iterator[A]) extends AnyVal {
+    /**
+     * Partitions this Iterator into a map according to a discriminator function `key`. All the values that
+     * have the same discriminator are then transformed by the `value` function and then reduced into a
+     * single value with the `reduce` function.
+     *
+     * {{{
+     *   def occurrences[A](as: Iterator[A]): Map[A, Int] =
+     *     as.groupMapReduce(identity)(_ => 1)(_ + _)
+     * }}}
+     *
+     * @note This will force the evaluation of the Iterator.
+     */
+    def groupMapReduce[K, B](key: A => K)(f: A => B)(reduce: (B, B) => B): immutable.Map[K, B] = {
+      val m = mutable.Map.empty[K, B]
+      iter.foreach { elem =>
+        m.updateWith(key = key(elem)) {
+          case Some(b) => Some(reduce(b, f(elem)))
+          case None    => Some(f(elem))
+        }
+      }
+      m.to(immutable.Map)
+    }
+  }
+}
---
+++ src/test/scala/scala/collection/TestIteratorExtensions.scala
@@ -0,0 +1,29 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.collection
+
+import org.junit.Assert._
+import org.junit.Test
+import IteratorExtensions._
+
+final class TestIteratorExtensions {
+  @Test
+  def groupMapReduce(): Unit = {
+    def occurrences[A](as: Seq[A]): Map[A, Int] =
+      as.iterator.groupMapReduce(identity)(_ => 1)(_ + _)
+
+    val xs = Seq('a', 'b', 'b', 'c', 'a', 'a', 'a', 'b')
+    val expected = Map('a' -> 4, 'b' -> 3, 'c' -> 1)
+    assertEquals(expected, occurrences(xs))
+  }
+}
