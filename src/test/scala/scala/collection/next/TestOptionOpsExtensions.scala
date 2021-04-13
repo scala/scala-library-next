92c0bb9eead5b2ab5919e8f097f02f74aeed7e95
georg <gdiet@users.noreply.github.com>
2021-04-13 09:28:54 +0200

    Specialised Option.tapEach to preserve the type

---
+++ src/test/scala/scala/collection/next/TestOptionOpsExtensions.scala
@@ -0,0 +1,21 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.collection.next
+
+import org.junit.Assert._
+import org.junit.Test
+
+final class TestOptionOpsExtensions {
+  // Compile checks the return type, no need to run as test.
+  def tapEachReturnType(): Option[Int] = Option(5).tapEach(identity)
+}
